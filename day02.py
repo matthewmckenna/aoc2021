689039d14f868b1b882ac29b7c641dc77c72d587
Matthew McKenna <mattheweb.mckenna@gmail.com>
2021-12-17 01:50:30 +0000

    complete day02

--- day02.py
+++ day02.py
@@ -13,10 +13,37 @@ def main():
     filepath = Path('data/day02.txt')
 
     print(f'part1: {part_one(filepath)}')
-    # print(f'part2: {part_two(filepath)}')
+    print(f'part2: {part_two(filepath)}')
 
 
-def part_one(filepath: Path):
+def part_two(filepath: Path) -> int:
+    """return solution to part 2"""
+    instructions = get_lines(filepath)
+    # forward X
+    # - increases horizontal position by X
+    # - increases depth by aim * X
+    # down X
+    # - increases aim by X
+    # up X
+    # - decreases aim by X
+    horizontal, depth, aim = 0, 0, 0
+
+    for line in instructions:
+        movement, magnitude = line.split()
+        mag = int(magnitude)
+
+        if movement == 'forward':
+            horizontal += mag
+            depth += mag * aim
+        elif movement == 'down':
+            aim += mag
+        elif movement == 'up':
+            aim -= mag
+
+    return horizontal * depth
+
+
+def part_one(filepath: Path) -> int:
     """return solution to part 1"""
     instructions = get_lines(filepath)
     # forward increases horizontal position
